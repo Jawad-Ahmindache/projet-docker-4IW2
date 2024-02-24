e0045c713c6fd2accca286fd622a056bba695502
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-02-24 22:38:34 +0100

    composer dockerfile
    Signed-off-by: Ahmindache Jawad <jawad.ahmindache@gmail.com>

---
+++ .docker/symfony/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d "./src" ]; then
+    echo "Le répertoire 'src' n'existe pas. Création d'un nouveau projet Symfony..."
+    symfony new src
+else
+    echo "Le répertoire 'src' existe déjà."
+fi
+
+symfony server:start --no-tls --port=8000 --daemon
+tail -f /dev/null
