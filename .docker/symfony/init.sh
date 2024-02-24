4d92568f2abbb7ace4115b6f6d5121bec3bd74b1
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-02-24 22:57:43 +0100

    symfony src
    Signed-off-by: Ahmindache Jawad <jawad.ahmindache@gmail.com>

--- .docker/symfony/init.sh
+++ .docker/symfony/init.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "./src" ]; then
     echo "Le répertoire 'src' n'existe pas. Création d'un nouveau projet Symfony..."
-    symfony new app
+    symfony new src
 else
     echo "Le répertoire 'src' existe déjà."
 fi
