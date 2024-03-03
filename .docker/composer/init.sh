accaf3d61a59f26bc682b58feb6cd7ea2012465d
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-03-03 22:26:22 +0100

    symfony code

--- .docker/composer/init.sh
+++ .docker/composer/init.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 composer install
 composer dump-autoload
-composer require symfony/maker-bundle --dev
-composer require symfony/orm-pack
 composer require doctrine/doctrine-fixtures-bundle --dev
