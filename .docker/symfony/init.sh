b8f097ca96c7fdd6d9481988a6cda196a33b2669
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-03-04 01:30:59 +0100

    docker

--- .docker/symfony/init.sh
+++ .docker/symfony/init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+sleep 3
 if [ ! -d "./src" ]; then
     echo "Le répertoire 'src' n'existe pas. Création d'un nouveau projet Symfony..."
     symfony new . --webapp
 else
     echo "Le répertoire 'src' existe déjà."
 fi
+php bin/console d:d:d --force
+php bin/console d:d:c
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate
+sleep 2
+php bin/console doctrine:fixtures:load
 symfony server:start --no-tls --port=8000 --daemon
 tail -f /dev/null
